99ba696afa171a27c1f8b58c83724631d6925b19
zen0bit <kvvark@gmail.com>
2019-02-26 22:10:15 +0000

    Create installnixos.sh

---
+++ installnixos.sh
@@ -0,0 +1,7 @@
+mount /dev/disk/by-label/nixos /mnt
+mkfs.fat -F 32 -n boot /dev/sdc1
+mkdir -p /mnt/boot
+mount /dev/disk/by-label/boot /mnt/boot
+nixos-generate-config --root /mnt
+nano /mnt/etc/nixos/configuration.nix
+nixos-install --show-trace
