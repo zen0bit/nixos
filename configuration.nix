39b0f821f92e3904a2e875fe6d19cc7207a7a996
zen0bit <kvvark@gmail.com>
2019-02-26 22:45:52 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -23,6 +23,10 @@
   # Define on which hard drive you want to install Grub.
    boot.loader.grub.device = "nodev"; # or "nodev" for efi only
 
+# Use the systemd-boot EFI boot loader.
+# boot.loader.systemd-boot.enable = true;
+# boot.loader.efi.canTouchEfiVariables = true;
+
    networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
