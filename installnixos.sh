5687ab759a72b5db7dcb160463c090614da3acd9
zen0bit <kvvark@gmail.com>
2019-02-27 03:18:56 +0100

    Delete installnixos.sh

--- installnixos.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-  mount /dev/disk/by-label/nixos /mnt
-  mkfs.fat -F 32 -n boot /dev/sdc1
-  mkdir -p /mnt/boot
-  mount /dev/disk/by-label/boot /mnt/boot
-# nixos-generate-config --root /mnt
- curl https://raw.githubusercontent.com/zen0bit/nixos/master/configuration.nix -o /mnt/etc/nixos/configuration.nix
-  nano /mnt/etc/nixos/configuration.nix
-  nixos-install --show-trace
