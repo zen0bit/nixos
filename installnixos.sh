ec66757c69b406cc1a7b9986a1ae3e59edaccd69
zen0bit <kvvark@gmail.com>
2019-02-27 03:53:06 +0100

    Update installnixos.sh

--- installnixos.sh
+++ installnixos.sh
@@ -2,7 +2,10 @@
   mount /dev/disk/by-label/nixos /mnt
   mkdir -p /mnt/boot
   mount /dev/disk/by-label/boot /mnt/boot
- nixos-generate-config --root /mnt
- curl https://raw.githubusercontent.com/zen0bit/nixos/master/configuration.nix -o /mnt/etc/nixos/configuration.nix
+  
+  nixos-generate-config --root /mnt
+  curl https://raw.githubusercontent.com/zen0bit/nixos/master/configuration.nix -o /mnt/etc/nixos/configuration.nix
   nano /mnt/etc/nixos/configuration.nix
+  
   nixos-install --show-trace
+  passwd zen
