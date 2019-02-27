63bf7a7cb6da849c0e93ebeb4e27a0f4370acba5
zen0bit <kvvark@gmail.com>
2019-02-27 04:55:16 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -87,7 +87,7 @@
 
   # Enable sound.
    sound.enable = true;
-  # hardware.pulseaudio.enable = true;
+   hardware.pulseaudio.enable = true;
 
   # Enable the X11 windowing system.
    services.xserver.enable = true;
